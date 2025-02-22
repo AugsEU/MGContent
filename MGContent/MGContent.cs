64547d0ebc7eb72af4e0cd6d53564d30cd38e1d0
August Dahlkvist <AugustDahlkvist@gmail.com>
2025-02-22 17:57:02 +0100

    New monogame version

---
+++ MGContent/MGContent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MGContent;
+
+public class MGContent : Game
+{
+	private GraphicsDeviceManager _graphics;
+
+	public MGContent()
+	{
+		_graphics = new GraphicsDeviceManager(this);
+		Content.RootDirectory = "Content";
+		IsMouseVisible = true;
+	}
+
+	protected override void Initialize()
+	{
+		base.Initialize();
+	}
+
+	protected override void LoadContent()
+	{
+	}
+
+	protected override void Update(GameTime gameTime)
+	{
+		if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+			Exit();
+
+		base.Update(gameTime);
+	}
+
+	protected override void Draw(GameTime gameTime)
+	{
+		GraphicsDevice.Clear(Color.CornflowerBlue);
+
+		// TODO: Add your drawing code here
+
+		base.Draw(gameTime);
+	}
+}
